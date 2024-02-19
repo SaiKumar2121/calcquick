6058d8533e1f9c61d20a9317b72568a0fc9ddc52
saikumar <saikumar223359@gmail.com>
2024-02-19 18:04:12 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# CalcQuick
+
+Easy way to handle all your maths operations
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+To install CalcQuick, simply run the following command in your terminal:
+
+```bash
+npm install calcquick
+```
+
+## Usage
+
+To use CalcQuick, simply require it in your JavaScript file:
+
+```javascript
+const CalcQuick = require("calcquick");
+```
+
+You can then use the various methods available on the `CalcQuick` object to perform calculations. For example:
+
+```javascript
+const result = CalcQuick.add(2, 3);
+console.log(result); // 5
+```
+
+## License
+
+This project is licensed under the ISC License.
+
+## Contributing
+
+Contributions are always welcome! Please create an issue or pull request to discuss any changes you wish to make.
+
+## Tests
+
+To run the tests, simply run the following command in your terminal:
+
+```bash
+npm test
+```
+
+## Questions
+
+If you have any questions about the project, feel free to contact me at:
+
+- Email: <saikumar223359@gmail.com>
+- GitHub: [saikumar](https://github.com/SaiKumar2121)
+- LinkedIn: [saikumar](https://www.linkedin.com/in/saikumarkammakatla/)
+
+## Acknowledgements
+
+This project was inspired by the need for an easy-to-use, fast, and reliable maths operations library. I would like to thank Tabnine for training me and providing me with the necessary skills to assist you in your work.
---
+++ index.js
@@ -0,0 +1,82 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    if(b === 0) throw new Error("Cannot divide by zero");
+    return a / b;
+}
+function trigonometry(operation, angle, precision = 2) {
+    // Convert angle from degrees to radians for sine, cosine, and tangent
+    let radians = angle * (Math.PI / 180);
+    let result;
+  
+    switch (operation) {
+      case 'sin':
+        result = Math.sin(radians);
+        break;
+      case 'cos':
+        result = Math.cos(radians);
+        break;
+      case 'tan':
+        result = Math.tan(radians);
+        break;
+      case 'asin':
+        // For asin, acos, and atan, the input is not converted and the output is in degrees
+        result = Math.asin(angle) * (180 / Math.PI);
+        break;
+      case 'acos':
+        result = Math.acos(angle) * (180 / Math.PI);
+        break;
+      case 'atan':
+        result = Math.atan(angle) * (180 / Math.PI);
+        break;
+      default:
+        return 'Unknown operation';
+    }
+  
+    // Round the result to the specified precision
+    return Math.round(result * Math.pow(10, precision)) / Math.pow(10, precision);
+}
+function factorial(n) {
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+function mean(numbers) {
+  const sum = numbers.reduce((acc, cur) => acc + cur, 0);
+  const mean = sum / numbers.length;
+  return Math.round(mean * 10) / 10; // Rounds to 1 decimal place
+}
+function median(numbers) {
+  const sortedNumbers = [...numbers].sort((a, b) => a - b);
+  const midIndex = Math.floor(sortedNumbers.length / 2);
+
+  if (sortedNumbers.length % 2 === 0) { // If even number of elements
+    return (sortedNumbers[midIndex - 1] + sortedNumbers[midIndex]) / 2;
+  } else { // If odd number of elements
+    return sortedNumbers[midIndex];
+  }
+}
+function mode(numbers) {
+  const numberCounts = numbers.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
+  const maxCount = Math.max(...Object.values(numberCounts));
+  const modes = Object.keys(numberCounts).filter(
+    number => numberCounts[number] === maxCount
+  );
+
+  return modes.length === 1 ? Number(modes[0]) : modes.map(Number);
+}
+
+module.exports = { add, subtract, multiply, divide, trigonometry,factorial, mean, median,mode};
+  
